5c090ad4d38f02d004aa8e5a822df0bae4c97f63
wangsijun <wangsijun@xiaoxiangyoupin.cn>
2020-04-10 10:35:37 +0800

    新增组件

---
+++ src/views/head/head.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import logo from '../../logo.svg';
+// import { render } from '@testing-library/react'
+import './head.css'
+
+function Head() {
+    return (
+        <div className="head">
+            <div className="lg"></div>
+            <span>
+                Edit <code>src/App.js</code> and save to reload.
+            </span>
+      </div>
+    )
+}
+
+export default Head
---
+++ src/views/layout/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { render } from '@testing-library/react'
+
+function Layout() {
+    return (
+        <div className="layout">
+            <div className="top">top logo</div>
+            <div className="main-contain">
+                this is main-contain
+            </div>
+            <footer>
+                this is footer
+            </footer>
+        </div>
+    )
+}
+
+export default Layout
