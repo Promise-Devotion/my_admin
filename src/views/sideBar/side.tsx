78a7958873a90b1a46f7a6f2a9259a0ca3b4d3b6
wangsijun <wangsijun@xiaoxiangyoupin.cn>
2020-04-17 14:25:14 +0800

    class->hook

--- src/views/sideBar/side.tsx
+++ src/views/sideBar/side.tsx
@@ -5,79 +5,83 @@ import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/ico
 import 'antd/dist/antd.css';
 
 const { SubMenu } = Menu
-class Sider extends React.Component {
+function Sider() {
   // submenu keys of first level
-  rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+  const [rootSubmenuKeys, setrootSubmenuKeys] = useState(['sub1', 'sub2', 'sub4'])
+  const [openKeys, setopenKeys] = useState(['sub1'])
 
-  state = {
-    openKeys: ['sub1']
-  };
-
-  onOpenChange = (openKeys: any[]) => {
-    const latestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
-    if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-      this.setState({ openKeys });
+  const onOpenChange = (openKeys: any[]) => {
+    const latestOpenKey = openKeys.find(key => openKeys.indexOf(key) === -1);
+    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setopenKeys(openKeys);
     } else {
-      this.setState({
-        openKeys: latestOpenKey ? [latestOpenKey] : [],
-      });
+      const l = latestOpenKey ? [latestOpenKey] : []
+      openKeys = l
+      setopenKeys(openKeys)
     }
   };
-
-  render() {
-    return (
-      <Menu
-        mode="inline"
-        openKeys={this.state.openKeys}
-        onOpenChange={this.onOpenChange}
-        style={{ width: 256 }}
-      >
-        <SubMenu
-          key="sub1"
-          title={
-            <span>
-              <MailOutlined />
-              <span>菜单一</span>
-            </span>
-          }
-        >
-          <Menu.Item key="1">Option 1</Menu.Item>
-          <Menu.Item key="2">Option 2</Menu.Item>
-          <Menu.Item key="3">Option 3</Menu.Item>
-          <Menu.Item key="4">Option 4</Menu.Item>
-        </SubMenu>
-        <SubMenu
-          key="sub2"
-          title={
-            <span>
-              <AppstoreOutlined />
-              <span>菜单二</span>
-            </span>
-          }
-        >
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-          <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
-        </SubMenu>
-        <SubMenu
-          key="sub4"
-          title={
-            <span>
-              <SettingOutlined />
-              <span>菜单三</span>
-            </span>
-          }
-        >
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <Menu.Item key="11">Option 11</Menu.Item>
-          <Menu.Item key="12">Option 12</Menu.Item>
-        </SubMenu>
-      </Menu>
-    );
+  const fetchData = (data: []) => {
+    setTimeout(() => {
+      setrootSubmenuKeys(['sub5', 'sub6', 'sub7'])
+    }, 200);
   }
+  useEffect(() => {
+    onOpenChange(openKeys)
+  }, [openKeys])
+
+  return (
+    <Menu
+      mode="inline"
+      openKeys={openKeys}
+      onOpenChange={onOpenChange}
+      style={{ width: 200 }}
+    >
+      <SubMenu
+        key="sub1"
+        title={
+          <span>
+            <MailOutlined />
+            <span>菜单一</span>
+          </span>
+        }
+      >
+        <Menu.Item key="1">Option 1</Menu.Item>
+        <Menu.Item key="2">Option 2</Menu.Item>
+        <Menu.Item key="3">Option 3</Menu.Item>
+        <Menu.Item key="4">Option 4</Menu.Item>
+      </SubMenu>
+      <SubMenu
+        key="sub2"
+        title={
+          <span>
+            <AppstoreOutlined />
+            <span>菜单二</span>
+          </span>
+        }
+      >
+        <Menu.Item key="5">Option 5</Menu.Item>
+        <Menu.Item key="6">Option 6</Menu.Item>
+        <SubMenu key="sub3" title="Submenu">
+          <Menu.Item key="7">Option 7</Menu.Item>
+          <Menu.Item key="8">Option 8</Menu.Item>
+        </SubMenu>
+      </SubMenu>
+      <SubMenu
+        key="sub4"
+        title={
+          <span>
+            <SettingOutlined />
+            <span>菜单三</span>
+          </span>
+        }
+      >
+        <Menu.Item key="9">Option 9</Menu.Item>
+        <Menu.Item key="10">Option 10</Menu.Item>
+        <Menu.Item key="11">Option 11</Menu.Item>
+        <Menu.Item key="12">Option 12</Menu.Item>
+      </SubMenu>
+    </Menu>
+  );
 }
+
 export default Sider
